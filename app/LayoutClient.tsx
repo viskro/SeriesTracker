addbd5d20ef2f7cb2d6fea55c849b73b666aa1dc
Axel Malotiaux <amalotiaux@gmail.com>
2025-06-06 02:26:51 +0200

    hook useSession

--- app/LayoutClient.tsx
+++ app/LayoutClient.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 import Header from "@/features/layout/components/Header";
 import Footer from "@/features/layout/components/Footer";
 import { Toaster } from "sonner";
-import { useSessionSync } from "@/lib/hooks/useSession";
+import { useSessionSync } from "@/shared/hooks/useSession";
 
 export function LayoutClient({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
